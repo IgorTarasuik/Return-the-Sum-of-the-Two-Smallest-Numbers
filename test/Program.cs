2b8126a2be257d3716334fdeb84944b9cc31f676
Igor Tarasuik <ihortarasyk@gmail.com>
2020-05-06 22:42:41 +0300

    Add project files.

---
+++ test/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] check = { 2, 9, 6, -1 };
+            SumSmallest(check);
+
+        }
+
+        public static int SumSmallest(int[] values)
+        {
+
+            
+            IEnumerable<int> resultPositive = values.Where(i => i >= 0);
+            int[] resultFiltered = resultPositive.OrderBy(i => i).ToArray();
+            int resultFinal = resultFiltered[0] + resultFiltered[1];
+            return resultFinal; 
+        }
+
+          
+    }
+
+   
+}
