c3e2796d69276fabde03951c9355dff7bdeb8571
Igor Tarasuik <ihortarasyk@gmail.com>
2020-05-09 17:28:52 +0300

    String reverse

--- test/Program.cs
+++ test/Program.cs
@@ -10,9 +10,10 @@ namespace test
     {
         static void Main(string[] args)
         {
-            int[] check = { 2, 9, 6, -1 };
-            SumSmallest(check);// Return the Sum of the Two Smallest Numbers
+            //int[] check = { 2, 9, 6, -1 };
+            // SumSmallest(check);// Return the Sum of the Two Smallest Numbers
 
+            //Reverse("Andrey"); // reverse string 
         }
 
         public static int SumSmallest(int[] values)
@@ -23,6 +24,12 @@ namespace test
             int[] resultFiltered = resultPositive.OrderBy(i => i).ToArray(); // ordering numbers in asc order
             int resultFinal = resultFiltered[0] + resultFiltered[1];
             return resultFinal;
+        }
+
+        public static string Reverse(string str)
+        {
+            string reversedString = new string(str.Reverse().ToArray()); // reverse string 
+            return reversedString;
         }
     }
 
